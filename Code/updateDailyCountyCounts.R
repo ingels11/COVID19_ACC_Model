8244e6eb83f8954d451477cb662424309939a1d7
Ishaan Dave <idave@uga.edu>
2020-03-26 00:32:33 -0400

    added commments

--- Code/updateDailyCountyCounts.R
+++ Code/updateDailyCountyCounts.R
@@ -1,21 +1,33 @@
-
+## This code is run daily to ensure that number of cases in our working dataset is properly pulled from the 
+## Georgia DPH website with number of confirmed cases in each county
 library(rvest)
 install.packages("lubridate")
 library(lubridate)
+
+# Tell which website to look in / scrape HTML cod 
 dailyCountWebsite <- read_html("https://dph.georgia.gov/covid-19-daily-status-report")
 
+
+# Telling R to grab only the TABLES from the website
 tbls <- html_nodes(dailyCountWebsite, "table")
 
-head(tbls)
+
+# head(tbls)
+
+# From looking at the website, the table we want is the 3rd table on the site
 tbls_ls <- dailyCountWebsite %>%
   html_nodes("table") %>%
-  .[3] %>%
+  .[3] %>%                    ## Indicating we want the 3rd table
   html_table(fill = TRUE)
 
+# Creating a dataframe from the picked table
 countyCasesDaily = as.data.frame(tbls_ls)
 
-countyCasesDaily$date = ymd(Sys.Date())
 
+# countyCasesDaily$date = ymd(Sys.Date())
+
+
+# Reading in our working (full) dataset of all counties that we want to populate 
 countsToBeUpdated = read.csv("Data/ACC Healthcare Region Simulation  - Case Counts by County GA.csv")
 countsToBeUpdated[is.na(countsToBeUpdated)] <- 0
 names(countsToBeUpdated)[1] = "date"
@@ -29,6 +41,8 @@ countsToBeUpdated$date = lubridate::ymd(countsToBeUpdated$date)
 library(tidyr)
 library(plyr)
 
+
+# going through DPH website and populating the number of cases for each county in our working dataset
 for (i in 1:nrow(countyCasesDaily)){
   
   if (countyCasesDaily$County[i] %in%(names(countsToBeUpdated))){
@@ -38,9 +52,7 @@ for (i in 1:nrow(countyCasesDaily)){
   }
 }
 
-
-
-
+# Overwriting our working (full) dataset with up to date case numbers for each county in GA
 write.csv(countsToBeUpdated, "Data/ACC Healthcare Region Simulation  - Case Counts by County GA.csv", row.names = F)
 
 
@@ -50,7 +62,10 @@ write.csv(countsToBeUpdated, "Data/ACC Healthcare Region Simulation  - Case Coun
 
 
 
-
+## This bit of code just goes through and populates counties in the Athens area -- primary and secondary
+## I guess we could just subset the full working dataset above and indicate which we want, but 
+## to minimize that work, I just made a CSV file with only the primary / secondary counties and put an 
+## indicator variable to tell which kind (include in both primary AND sec or just secondary)
 
 
 primSecCounties = read.csv("Data/primary and secondary counties cases.csv")
