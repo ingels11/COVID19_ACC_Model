25d47365d9c0178e126dc63512ce12dc55ac3761
Ishaan Dave <idave@uga.edu>
2020-03-26 17:01:07 -0400

    update

--- Code/athens_simulations_secondary_non_ga_scaling.R
+++ Code/athens_simulations_secondary_non_ga_scaling.R
@@ -35,7 +35,7 @@ ggplot(data = acc_df, mapping = aes(x = Date, y = primary)) +
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
 # Plot of cumulative Athens cases
-ggplot(data = acc_df, mapping = aes(x = Date, y = primary_cum)) +
+ggplot(data = acc_df, mapping = aes(x = date, y = primary_cum)) +
   geom_bar(stat = "identity") +
   labs(x = "Day",
        y = "Cumulative Cases (Primary Service Area)") +
@@ -67,49 +67,6 @@ scenarios <- scenarios[, c(1:10, 13:31, 11, 12)]
 # ACC-area cases began on 3/14/20
 # Major intervention 3/20/20 with shelter in place, etc. from government
 # Intervention days then are 6 (w)
-scen_row <- 6
-
-# If nationally was 3/12/20 then this is prior to ACC outbreak so z = 0
-gamma <- function(z = scenarios[scen_row, "z"], b=scenarios[scen_row, "b"], a0=scenarios[scen_row, "a0"], t){
-  # piecewise function
-  # default parameters z = 12, b=1/7, a0=1/1.5
-  #    z: time at start of intervention (notionally March 12)
-  #    b: intercept (positive)
-  #    a0: post intervention isolation ratae
-  #    t: time in the model
-  
-  gamma <- ifelse(t<=z, gamma <- b, gamma <- a0)
-  return(gamma)
-}
-eta <- function(t, w = scenarios[scen_row, "w"]) ifelse(t<=w, 1/3, 1/3)
-q <- function(t, w = scenarios[scen_row, "w"], q0=scenarios[scen_row, "q0"], q1=scenarios[scen_row, "q1"]) ifelse(t<=w, q0, q1)
-beta <- function(t, w = scenarios[scen_row, "w"], beta0=scenarios[scen_row, "beta0"], beta.factor=2) {
-  ifelse(t<=w, beta0, beta0 / beta.factor)
-} 
-
-# First ACC-area case on 3/14/20
-start = as.Date("2020-03-14")
-
-
-# ei <- 13
-# ii <- 14
-# 
-# out6 <- evaluate.model(params=list(beta0=0.6584, sigma=1/6.4, z=1200, b=0.143, a0=1/1.5, w=100, presymptomatic=1, c=1, dt=0.05),
-#                        init = list(S=447451, E1=ei, E2=ei, E3=ei, E4=ei, E5=ei, E6=ei,
-#                                    I1 = ii, I2 = ii, I3 = ii, I4 = ii, Iu1=0, Iu2=0, Iu3=0, Iu4=0,
-#                                    H=0, Ru=0, C=0),
-#                        nsims=15, nstep=NULL, start=start)
-s <- scenarios[,3:31]
-i <- scen_row
-out6 <- evaluate.model(params=list(beta0=s[i,1], sigma=s[i,2], z=s[i,3], b=s[i,4], a0=s[i,5], w=s[i,6], presymptomatic=s[i,8], c=s[i,7], dt=s[i,9]),
-                       init = list(S=s[i,10], E1=s[i,11], E2=s[i,12], E3=s[i,13], E4=s[i,14], E5=s[i,15], E6=s[i,16],
-                                   I1 = s[i,17], I2 = s[i,18], I3 = s[i,19], I4 = s[i,20], Iu1=s[i,21], Iu2=s[i,22], Iu3=s[i,23], Iu4=s[i,24],
-                                   H=s[i,25], Ru=s[i,26], C=s[i,27]),
-                       nsims=15, nstep=NULL, start=start)
-
-
-plot.model(out6, log='y', title='Benchmark: Natural epidemic')
-
 
 ### Baseline Model (Scenario 7) ------------------------------------------------
 # out.base <- evaluate.model(params=list(beta0=0.6584, sigma=1/6.4, z=0, b=0.143, a0=1/1.5, w=100, c=1, presymptomatic=1, dt=0.05),
@@ -178,7 +135,7 @@ out8 <- evaluate.model(params=list(beta0=s[i,1], sigma=s[i,2], z=s[i,3], b=s[i,4
                                    H=s[i,25], Ru=s[i,26], C=s[i,27]),
                        nsims=15, nstep=NULL, start=start)
 
-plot.model.acc(out8, log='y', title='With Social Distancing')
+plot.model(out8, log='y', title='With Social Distancing')
 
 
 ### Smaller and Larger Starting Sizes (Scenarios 3 and 5) ----------------------
@@ -217,6 +174,11 @@ plot.model(out3, log='y', title='With Social Distancing (Lower Bound)')
 
 # Bigger next
 scen_row <- 5
+
+
+
+
+
 
 # Too stringent, earlier assumptions likely make more sense
 # If nationally was 3/12/20 then this is prior to ACC outbreak so z = 0
