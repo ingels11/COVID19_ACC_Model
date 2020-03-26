735abdf980ead5bdd66089e8a56e2491cf671c5f
Ishaan Dave <idave@uga.edu>
2020-03-26 00:25:37 -0400

    updating to do only primary/secondary counties for future use

--- Code/updateDailyCountyCounts.R
+++ Code/updateDailyCountyCounts.R
@@ -1,7 +1,7 @@
 
 library(rvest)
 install.packages("lubridate")
-
+library(lubridate)
 dailyCountWebsite <- read_html("https://dph.georgia.gov/covid-19-daily-status-report")
 
 tbls <- html_nodes(dailyCountWebsite, "table")
@@ -18,6 +18,7 @@ countyCasesDaily$date = ymd(Sys.Date())
 
 countsToBeUpdated = read.csv("Data/ACC Healthcare Region Simulation  - Case Counts by County GA.csv")
 countsToBeUpdated[is.na(countsToBeUpdated)] <- 0
+names(countsToBeUpdated)[1] = "date"
 countsToBeUpdated$date = lubridate::ymd(countsToBeUpdated$date)
 
 
@@ -38,6 +39,42 @@ for (i in 1:nrow(countyCasesDaily)){
 }
 
 
+
+
 write.csv(countsToBeUpdated, "Data/ACC Healthcare Region Simulation  - Case Counts by County GA.csv", row.names = F)
+
+
+
+
+
+
+
+
+
+
+
+primSecCounties = read.csv("Data/primary and secondary counties cases.csv")
+primSecCounties[is.na(primSecCounties)] <- 0
+names(primSecCounties)[1] = "date"
+primSecCounties$date = lubridate::ymd(primSecCounties$date)
+
+
+library(tidyr)
+library(plyr)
+
+for (i in 1:nrow(countyCasesDaily)){
+  
+  if (countyCasesDaily$County[i] %in%(names(primSecCounties))){
+    
+    primSecCounties[primSecCounties$date == Sys.Date()-1, as.character(countyCasesDaily$County)[i]] = 
+      countyCasesDaily[countyCasesDaily$County == as.character(countyCasesDaily$County)[i], 2]
+  }
+}
+
+
+write.csv(primSecCounties, "Data/primary and secondary counties cases.csv", row.names = F)
+
+
+
 
 
