ea1c8321fe6f11777125148b8bb013e65581c67e
Justin Ingels <ingels@uga.edu>
2020-04-01 11:09:04 -0400

    More Updates
    Does include final report for Grace.

--- Code/extract_numbers.R
+++ Code/extract_numbers.R
@@ -3,45 +3,62 @@ library(readr)
 library(dplyr)
 library(magrittr)
 
-latest_date <- "2020-03-31"
+latest_date <- "2020-04-01"
 ### Read in and Combine Scenario Models ----------------------------------------
 # Add in marking of peak new cases and hospital capacity by scenario
 poor_df <- read_rds(paste0("Models/epidemic_base_hosp_", 
                            latest_date, ".rds")) %>%
   mutate(scenario = "poor")
-max(which(poor_df$Hosp_case_high == max(poor_df$Hosp_case_high)))
-max(which(poor_df$Hosp_capacity_high == max(poor_df$Hosp_capacity_high)))
-max(which(poor_df$Case_new == max(poor_df$Case_new)))
+poor_df$max_new_hosp <- NA
+poor_df$max_tot_hosp <- NA
+poor_df$max_new_case <- NA
+poor_df$max_new_hosp[max(which(poor_df$Hosp_new_high == max(poor_df$Hosp_new_high)))] <- 1
+poor_df$max_tot_hosp[max(which(poor_df$Hosp_tot_high == max(poor_df$Hosp_tot_high)))] <- 1
+poor_df$max_new_case[max(which(poor_df$Case_new == max(poor_df$Case_new)))] <- 1
 
 avg_df <- read_rds(paste0("Models/social_distance_upper_hosp_", 
                           latest_date, ".rds")) %>%
   mutate(scenario = "average")
-max(which(avg_df$Hosp_case_high == max(avg_df$Hosp_case_high)))
-max(which(avg_df$Hosp_capacity_high == max(avg_df$Hosp_capacity_high)))
-max(which(avg_df$Case_new == max(avg_df$Case_new)))
+avg_df$max_new_hosp <- NA
+avg_df$max_tot_hosp <- NA
+avg_df$max_new_case <- NA
+avg_df$max_new_hosp[max(which(avg_df$Hosp_new_high == max(avg_df$Hosp_new_high)))] <- 1
+avg_df$max_tot_hosp[max(which(avg_df$Hosp_tot_high == max(avg_df$Hosp_tot_high)))] <- 1
+avg_df$max_new_case[max(which(avg_df$Case_new == max(avg_df$Case_new)))] <- 1
 
 exc_df <- read_rds(paste0("Models/social_distance_base_hosp_", 
                           latest_date, ".rds")) %>%
   mutate(scenario = "excellent")
-max(which(exc_df$Hosp_case_high == max(exc_df$Hosp_case_high)))
-max(which(exc_df$Hosp_capacity_high == max(exc_df$Hosp_capacity_high)))
-max(which(exc_df$Case_new == max(exc_df$Case_new)))
+exc_df$max_new_hosp <- NA
+exc_df$max_tot_hosp <- NA
+exc_df$max_new_case <- NA
+exc_df$max_new_hosp[max(which(exc_df$Hosp_new_high == max(exc_df$Hosp_new_high)))] <- 1
+exc_df$max_tot_hosp[max(which(exc_df$Hosp_tot_high == max(exc_df$Hosp_tot_high)))] <- 1
+exc_df$max_new_case[max(which(exc_df$Case_new == max(exc_df$Case_new)))] <- 1
 
 df <- bind_rows(poor_df, avg_df, exc_df) %>%
   mutate(scenario = factor(scenario, 
                            levels = c("poor", "average", "excellent"),
                            ordered = TRUE))
 rm(poor_df, avg_df, exc_df)
+df[is.na(df)] <- 0
 
 ### Extract Data ---------------------------------------------------------------
 df %>%
   group_by(scenario) %>%
   summarise(total_cases = max(Case_tot),
-            total_hosps = max(Hosp_tot_high),
-            total_crit_low = max(Crit_tot_low),
-            total_crit_high = max(Crit_tot_high),
-            max_capacity = max(Hosp_capacity_high),
-            total_deaths = max(Dead_tot_high))
+            total_hosps_low = sum(Hosp_new_low),
+            total_hosps_high = sum(Hosp_new_high),
+            max_capacity_low = max(Hosp_tot_low),
+            max_capacity_high = max(Hosp_tot_high),
+            max_capacity_crit_low = max(Crit_tot_low),
+            max_capacity_crit_high = max(Crit_tot_high))
 
-# Determine peak dates
+# Determine peak dates (high scenario only)
+df %>%
+  filter(max_tot_hosp == 1) %>%
+  select(scenario, Dates)
 
+df %>%
+  filter(max_new_case == 1) %>%
+  select(scenario, Dates)
---
+++ Code/modeling_flow_new.R
@@ -0,0 +1,88 @@
+# Update the daily county of cases in ACC and sorrounding area
+# writes to "primary and secondary counties cases.csv" and
+#   "primSecNewCasesDaily.csv"
+# source("Code/updateDailyCountyCounts.R")
+
+rm(list = ls())
+source("Code/model_fncs.R")
+
+# run models
+# models found in athens_simulations_secondary_ga_scaling.R
+# evaluate.model() -> model() -> onestep()
+# save one or more models, will include most up to date counts if 
+#   updateDailyCountyCounts.R has been run
+# Should save model as .rds (with date as part of title) for further processing
+#   * social_distance_base_
+
+# analyze models
+# run the following functions using the saved .rds files
+# each produces an output that should be fed to the next
+# model_hospitalizations()
+# summarise_model_hospitalizations()
+# hospital_capacity()
+
+latest_date <- "2020-04-01"
+
+# Social distancing works well
+# 15 simulations of the base social distancing model
+base_mod <- read_rds(paste0("Models/social_distance_base_new_", latest_date, ".rds"))
+base_hosp_mod <- model_hospitalizations(base_mod)
+base_hosp_mod <- summarise_model_hospitalizations(base_hosp_mod)
+base_hosp_mod <- hospital_capacity(base_hosp_mod)
+write_rds(base_hosp_mod, paste0("Models/social_distance_base_hosp_", 
+                                latest_date, ".rds"))
+
+# Plot results
+cumCases <- read_csv("Data/primary and secondary counties cases.csv")
+cumCases$secondary <- rowSums(cumCases[,2:18])
+#cumCases %<>% filter(date <= Sys.Date())
+cumCases %<>% filter(date <= as.Date("2020-03-31"))
+plot.model.acc(base_mod,  cumCases$date, cumCases$secondary,
+               log='y', title='Model With Excellent Social Distancing')
+plot_hospitalizations(base_hosp_mod, type = "cum", 
+                      title = "Model with Excellent Social Distancing (Total Hospitalization Count)")
+ggsave(paste0("Plots/exc_socdist_hosp_total_", Sys.Date(), ".png"))
+plot_hospitalizations(base_hosp_mod, type = "capacity",
+                      title = "Model with Excellent Social Distancing (Current Hospitalization Count)")
+ggsave(paste0("Plots/exc_socdist_hosp_current_", Sys.Date(), ".png"))
+plot_hospitalizations(base_hosp_mod, type = "pct")
+
+# Social distancing works not as well
+# 15 simulations of the base social distancing model
+upper_mod <- read_rds(paste0("Models/social_distance_base_", latest_date, ".rds"))
+upper_hosp_mod <- model_hospitalizations(upper_mod)
+upper_hosp_mod <- summarise_model_hospitalizations(upper_hosp_mod)
+upper_hosp_mod <- hospital_capacity(upper_hosp_mod)
+write_rds(upper_hosp_mod, paste0("Models/social_distance_upper_hosp_", 
+                                latest_date, ".rds"))
+
+# Plot results
+plot.model.acc(upper_mod,  cumCases$date, cumCases$secondary,
+               log='y', title='Model With Average Social Distancing')
+plot_hospitalizations(upper_hosp_mod, type = "cum", 
+                      title = "Model with Average Social Distancing (Total Hospitalization Count)")
+ggsave(paste0("Plots/avg_socdist_hosp_total_", Sys.Date(), ".png"))
+plot_hospitalizations(upper_hosp_mod, type = "capacity",
+                      title = "Model with Average Social Distancing (Current Hospitalization Count)")
+ggsave(paste0("Plots/avg_socdist_hosp_current_", Sys.Date(), ".png"))
+
+
+# Natural epidemic
+epid_mod <- read_rds(paste0("Models/epidemic_base_", latest_date, ".rds"))
+epid_hosp_mod <- model_hospitalizations(epid_mod)
+epid_hosp_mod <- summarise_model_hospitalizations(epid_hosp_mod)
+epid_hosp_mod <- hospital_capacity(epid_hosp_mod)
+write_rds(epid_hosp_mod, paste0("Models/epidemic_base_hosp_", latest_date, ".rds"))
+
+acc_natural <- plot.model.acc(epid_mod, cumCases$date, cumCases$secondary,
+               log='y', title='Natural Epidemic Model')
+# ggsave(paste0("Plots/Athens_natural_epidemic_", Sys.Date(), ".png"), acc_natural)
+
+plot_hospitalizations(epid_hosp_mod, type = "cum", 
+                      title = "Model with Poor Social Distancing (Total Hospitalization Count)")
+ggsave(paste0("Plots/poor_socdist_hosp_total_", Sys.Date(), ".png"))
+plot_hospitalizations(epid_hosp_mod, type = "capacity",
+                      title = "Model with Poor Social Distancing (Current Hospitalization Count)")
+ggsave(paste0("Plots/poor_socdist_hosp_current_", Sys.Date(), ".png"))
+
+
